86f4e0bbf6fe44413e492ae6f5eddc780eb4d72a
Sebastijan Zindl <sebastijanzindl@protonmail.com>
2024-02-17 19:13:37 +0100

    feat: added daily & median sales view

---
+++ BookStore/Views/WeekdayStats/SalesByWeekdayHeader.swift
@@ -0,0 +1,18 @@
+//
+//  SalesByWeekdayHeader.swift
+//  BookStore
+//
+//  Created by Sebastijan Zindl on 17.2.24.
+//
+
+import SwiftUI
+
+struct SalesByWeekdayHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SalesByWeekdayHeader()
+}
